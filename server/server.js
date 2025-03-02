733b968179414be113dfe0913477d72f84699f04
Kunal Asude <kunalasude@gmail.com>
2025-03-02 18:42:52 +0530

    BackEnd Setup

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Test Route
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
