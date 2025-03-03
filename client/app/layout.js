47077435b88b42647540ee4d6449d40adac9bd3f
Kunal Asude <kunalasude@gmail.com>
2025-03-03 15:12:23 +0530

    meta update

--- client/app/layout.js
+++ client/app/layout.js
@@ -9,17 +9,23 @@ import { CartProvider } from "@/context/CartContext";
 const inter = Inter({ subsets: ["latin"] });
 export const dynamic = "force-dynamic";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children, title = "Medi-Store", description = "Your trusted online pharmacy" }) {
   return (
     <html lang="en">
+      <head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+      </head>
       <body className={inter.className}>
         <CartProvider>
-            <Navbar />
-            <main className="bg-[linear-gradient(to_right,#042F2E,#012621,#002A1C)]">{children}</main>
-            <footer className="py-4 text-center bg-teal-950 text-white">
-              © {new Date().getFullYear()} MediStore. All rights reserved.
-            </footer>
-         
+          <Navbar />
+          <main className="bg-[linear-gradient(to_right,#042F2E,#012621,#002A1C)]">{children}</main>
+          <footer className="py-4 text-center bg-teal-950 text-white">
+            © {new Date().getFullYear()} MediStore. All rights reserved.
+          </footer>
+
         </CartProvider>
       </body>
     </html>
