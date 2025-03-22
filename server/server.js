4662736ecabc90cb6a0a817acd499b25993833c1
SamarthKasar123 <samarthkasar9924@gmail.com>
2025-03-22 17:41:08 +0530

    Payment Details added

--- server/server.js
+++ server/server.js
@@ -29,10 +29,7 @@ app.use(cors({
 // Database Connection
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB Connected");
   } catch (err) {
     console.error("MongoDB Connection Error:", err);
